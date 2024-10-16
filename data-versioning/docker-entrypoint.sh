ac603bfe2bd8dddbc4a49541ae28a020cc816c2e
John Jun <111310290+JohnSpJun@users.noreply.github.com>
2024-10-16 18:35:51 -0400

    updated cheese_dataset to car_dataset in docker-entrypoint.sh

--- data-versioning/docker-entrypoint.sh
+++ data-versioning/docker-entrypoint.sh
@@ -2,12 +2,11 @@
 
 echo "Container is running!!!"
 
-
 gcloud auth activate-service-account --key-file=$GOOGLE_APPLICATION_CREDENTIALS
 mkdir -p /mnt/gcs_bucket
 gcsfuse --key-file=$GOOGLE_APPLICATION_CREDENTIALS $GCS_BUCKET_NAME /mnt/gcs_data
 echo 'GCS bucket mounted at /mnt/gcs_data'
-mkdir -p /app/cheese_dataset
-mount --bind /mnt/gcs_data/images /app/cheese_dataset
+mkdir -p /app/car_dataset
+mount --bind /mnt/gcs_data/images /app/car_dataset
 
 pipenv shell
